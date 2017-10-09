1a51f316958135c7d15b762a573f729628fe9e99
agiosZafiris <ZAFKARABOIKIS2@GMAIL.COM>
2017-10-09 19:03:26 +0300

    changed ball colors
    (i hope)

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#F02233", "#087FBD", "#FDBB30"]);
+var fill = d3.scale.ordinal().range(["#e8636e", "#0826bd", "#443004"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
