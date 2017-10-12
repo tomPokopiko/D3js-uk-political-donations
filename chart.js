918b102a2f832599d2b508cd7e0d82fd6f23265b
agiosZafiris <ZAFKARABOIKIS2@GMAIL.COM>
2017-10-12 15:42:05 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -93,7 +93,7 @@ function start() {
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
 		.on("mouseout", mouseout);
-		.ondblclick(function(d){ window.open("http://www.google.com/search?q=" + d.donor);})
+		.ondblclick(function(d){ window.open("http://www.google.com/search?q=" + d.donor);});
 		// Alternative title based 'tooltips'
 		// node.append("title")
 		//	.text(function(d) { return d.donor; });
